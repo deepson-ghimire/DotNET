fec2d6d2e0cc36c2134c9b3652d81f5e27cb8662
deepson-ghimire <deepsonghimire51@gmail.com>
2026-05-15 11:23:07 +0545

    new file

--- ObjectOriented/AbstractExample.cs
+++ ObjectOriented/AbstractExample.cs
@@ -1,54 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ObjectOriented
-{
-    internal class AbstractExample
-    {
-        public void run() {
-            Animal dog = new Dog();
-            dog.MakeSound();
-        }
-    }
-    public abstract class Animal
-    {
-        public abstract void MakeSound();
+//namespace ObjectOriented
+//{
+//    internal class AbstractExample
+//    {
+//        public void run() {
+//            Animal dog = new Dog();
+//            dog.MakeSound();
+//        }
+//    }
+//    public abstract class Animal
+//    {
+//        public abstract void MakeSound();
 
-        public void Eat() => Console.WriteLine("Animal Eating");
-    }
+//        public void Eat() => Console.WriteLine("Animal Eating");
+//    }
 
-    public class Dog : Animal
-    {
-        public override void MakeSound()
-        {
-            Console.WriteLine("Making sound");
-        }
-    }
+//    public class Dog : Animal
+//    {
+//        public override void MakeSound()
+//        {
+//            Console.WriteLine("Making sound");
+//        }
+//    }
 
-    // Class problem
-    public abstract class StudentBase
-    {
-        public abstract string Name { get; set; }
+//    // Class problem
+//    public abstract class StudentBase
+//    {
+//        public abstract string Name { get; set; }
 
-        public abstract void DisplayInfo();
+//        public abstract void DisplayInfo();
 
-        public abstract string this[int index] { get; set; }
-    }
+//        public abstract string this[int index] { get; set; }
+//    }
 
-    class Student2 : StudentBase {
-        private string name;
-        private string[] courses = new string[5];
-        public override string Name {
-            get { return this.name; }
-            set { name = value; }
-        }
+//    class Student2 : StudentBase {
+//        private string name;
+//        private string[] courses = new string[5];
+//        public override string Name {
+//            get { return name; }
+//            set { name = value; }
+//        }
 
-        public override void DisplayInfo() { 
-            
-        }
-    }
-}
+//        public override void DisplayInfo() {
+//            Console.WriteLine("Name: " + Name);
+
+//            for (int i = 0; i < courses.Length; i++)
+//            {
+//                if (courses[i] != null)
+//                {
+//                    Console.WriteLine("Course: " + courses[i]);
+//                }
+//            }
+//        }
+//    }
+//}
